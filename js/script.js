818c37545f36cf5785d0a156e57bd1bfcf1e5c09
Robert Rybiański <40652170+webeeq@users.noreply.github.com>
2020-04-30 12:27:10 +0200

    Change javascript method

--- js/script.js
+++ js/script.js
@@ -7,16 +7,7 @@ $(document).ready(function() {
 });
 
 function ajaxData(id, file) {
-    if (window.XMLHttpRequest) {
-        xmlhttp = new XMLHttpRequest();
-    } else {
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            document.getElementById(id).innerHTML = xmlhttp.responseText;
-        }
-    }
-    xmlhttp.open("GET", file, true);
-    xmlhttp.send();
+    $.get(file, function(data) {
+        $("#" + id).html(data);
+    });
 }
